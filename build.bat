923319f248537bdbfbd40d2f0db2f90f30983897
TheStef56 <stefanosanasi2017@gmail.com>
2025-03-26 19:28:35 +0100

    full windows working version

--- build.bat
+++ build.bat
@@ -1,1 +1,2 @@
 gcc -o trex main.c -g -I./include -L./lib/win32/x64 -lSDL2main -lSDL2 -lgdi32 -lSDL2_image -lSDL2_ttf -lSDL2_mixer -lm
+copy .\lib\win32\x64 .\
--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 set -xe
 
 #cd dependencies && make
-cc -o trex main.c -g -Wall -Wextra -I./include -L./lib -lSDL2 -lSDL2main -lSDL2_image -lSDL2_ttf -lSDL2_mixer -lm -Bstatic
+cc -o trex main.c -g -Wall -Wextra -I./include -L./lib -lSDL2 -lSDL2main -lSDL2_image -lSDL2_ttf -lSDL2_mixer -lm
