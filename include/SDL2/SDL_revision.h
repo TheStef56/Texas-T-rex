2d125c58eb7a7cac5d269010515fe54b46f378c2
TheStef56 <stefanosanasi2017@gmail.com>
2025-08-31 13:22:41 +0200

    added wayland/x11 support check in cmake

--- include/SDL2/SDL_revision.h
+++ include/SDL2/SDL_revision.h
@@ -2,7 +2,7 @@
 #define SDL_REVISION_NUMBER 0
 
 #ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-2.30.7-gef5051b (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION "SDL-release-2.30.12-2-g9464f334b (" SDL_VENDOR_INFO ")"
 #else
-#define SDL_REVISION "SDL-2.30.7-gef5051b"
+#define SDL_REVISION "SDL-release-2.30.12-2-g9464f334b"
 #endif
